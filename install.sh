160fd71be86046ce8a9fcd3501fe94a41edbc722
phasath <sathler93@gmail.com>
2018-12-20 10:10:11 -0200

    Created a function to display a msg and abort if command didnt work

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@ INSTALL_DIR=''
 COLLECTD_DIR=''
 DISTRO_PKG_MAN=''
 MACHINE_NAME=$1
+
 TMP_PATH=$(mktemp -d)
+if [ $? -eq 0 ]; then
+   	show_error "while creating temporary path."
+fi
 
 if [ -z $2 ]; then
 	GRAPHITE_IP='10.60.0.27'
@@ -14,15 +18,23 @@ fi
 
 trap "{ rm -rf $TMP_PATH; }" EXIT
 
+function show_error {
+	echo ""
+	echo ""
+	echo "A error happened while $1"
+	echo "Aborting!"
+	exit 1 ;
+}
+
 function getting_distro_info {
 	if [[ "$DISTRO" == *"Ubuntu"* ]]; then
 		INSTALL_DIR='/etc/collectd'
 		COLLECTD_DIR='/etc/collectd/collectd.conf.d'
-		DISTRO_PKG_MAN='apt-get -mfy'
+		DISTRO_PKG_MAN='apt-get -m -f -y'
 	elif [[ "$DISTRO" == *"CentOS"* ]] || [[ "$DISTRO" == *"Oracle"* ]]; then
 		INSTALL_DIR='/etc'
 		COLLECTD_DIR='/etc/collectd.d'
-		DISTRO_PKG_MAN='yum -qy'
+		DISTRO_PKG_MAN='yum -q -y'
 	else 
 		echo "No OS detected"
 		echo "Aborting..."
@@ -43,23 +55,49 @@ function starting_text {
 function updating_packages {
 	echo "---> Updating package lists"
 	eval "sudo $DISTRO_PKG_MAN update"
+	if [ $? -eq 0 ]; then
+    	show_error "updating package list."
+	fi
 }
 
 function dependencies_install {
-	echo "---> Installing necessaries dependencies"
+	echo "---> Installing necessary dependencies"
 	echo "-----> Collectd and Dependencies"
 	eval "sudo $DISTRO_PKG_MAN install collectd collectd-utils collectd-core collectd-ping liboping0 collectd-nginx"
+	if [ $? -eq 0 ]; then
+    	show_error "instlaling necessary dependencies."
+	fi
 }
 
 function configuring_collectd {
 	echo "-----> Configuring Collectd"
+
 	eval "cp ${PWD}/collectd/collectd.tmpl ${TMP_PATH}/collectd.conf"
+	if [ $? -eq 0 ]; then
+    	show_error "moving contents to temporary path."
+	fi
+
 	sed -i -e "s@{MACHINE_NAME}@${MACHINE_NAME}@" -e "s@{COLLECTD_DIR}@${COLLECTD_DIR}@" -e "s@{GRAPHITE_IP}@${GRAPHITE_IP}@" ${TMP_PATH}/collectd.conf 
+	if [ $? -eq 0 ]; then
+    	show_error "creating collectd config file."
+	fi
+
 	eval "sudo cp --force ${TMP_PATH}/collectd.conf ${INSTALL_DIR}/"
+	if [ $? -eq 0 ]; then
+    	show_error "copying collectd configuration file."
+	fi
+	
 	echo "-----> Creating log path for Collectd in /var/log/collectd"
 	eval "sudo mkdir -p /var/log/collectd"
+	if [ $? -eq 0 ]; then
+    	show_error "creating log path for collectd."
+	fi
+
 	echo "-----> Starting Collectd"
 	eval "sudo collectd -C ${INSTALL_DIR}/collectd.conf"
+	if [ $? -eq 0 ]; then
+    	show_error "starting collectd."
+	fi
 }
 
 getting_distro_info
